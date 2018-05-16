1f78c18eb2d746d567cb5a9cb5e02820e2893f4e
Jon Redeker <jon.redeker@gmail.com>
2018-05-16 15:43:23 -0400

    initial commit with new django app

---
+++ functional_tests/tests.py
@@ -0,0 +1,24 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+
+MAX_WAIT = 10
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_visit_site(self):
+        # user opens the web site
+        self.browser.get(self.live_server_url)
+        current_url = self.browser.current_url
+
+        # the page loads and sees the title
+        self.assertIn('Jon Redeker', self.browser.title)
---
+++ polls/tests.py
@@ -0,0 +1,12 @@
+from django.urls import resolve
+from django.test import TestCase
+from polls.views import *
+
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
